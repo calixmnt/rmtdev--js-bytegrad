b27ad335634cba998af2137207a72229db40406e
Calixxx <calixmonnet@gmail.com>
2024-08-30 15:51:26 +0000

    Build Process

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './components/JobList.js';
+import './components/Search.js';
+import './components/Error.js';
+import './components/Sorting.js';
+import './components/Pagination.js';
+import './components/Router.js';
+import './components/Bookmarks.js';
+import './components/Storage.js';
+
+
+import './index.css';
+
